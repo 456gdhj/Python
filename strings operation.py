79932fc4b996da74cbd97d78dbb7a3280182eb70
Md Salauddin <145477267+456gdhj@users.noreply.github.com>
2024-09-12 22:26:16 +0600

    Add files via upload

---
+++ strings operation.py
@@ -0,0 +1,15 @@
+str1="Md"
+str2="salauddin"
+print(len(str1))
+print(len(str2)) #print the size of string
+
+print(str1+str2) #concatinate string
+
+print(str2[0:4]) #print string accroding to index number
+
+print(str2.capitalize())#capital the first letter of string
+
+print(str2.replace("a","o")) #replace letter in a string
+
+
+    
