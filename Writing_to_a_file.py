7dee886fe890745b7e8263b6c240c0548fc31f51
Md Salauddin <145477267+456gdhj@users.noreply.github.com>
2024-09-12 22:27:33 +0600

    Add files via upload

---
+++ Writing_to_a_file.py
@@ -0,0 +1,11 @@
+#f=open("E:\Machine Learning\demo.txt","a")
+
+#f.write("\ni wanted to learn javascript")
+#f.close()
+
+#f=open("E:\Machine Learning\demo.txt","w")
+#f.write("I am a good boy")
+#f.close()
+
+import os
+os.remove("E:\Machine Learning\demo.txt")
