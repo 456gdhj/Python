9f980d827a30ca0b6a72af67a2dad3a4b27297e5
Md Salauddin <145477267+456gdhj@users.noreply.github.com>
2024-09-12 22:26:50 +0600

    Add files via upload

---
+++ Set operation.py
@@ -0,0 +1,11 @@
+collection={1,2,3,2,1,5,"sala","world","world","md"}  #set
+collection.add(7) #add element in set
+collection.remove(3) #remove element in set
+
+print(collection.pop()) #Give a random value
+print(collection)
+
+set1={1,3,5,9,4,7,7}
+set2={2,1,6,8,7}
+print(set1.union(set2)) #union of two set
+print(set1.intersection(set2))#intersection of two set
