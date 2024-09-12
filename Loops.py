4ed026e8aecd57b7b755e1aa5d65d88e0c846c63
Md Salauddin <145477267+456gdhj@users.noreply.github.com>
2024-09-12 22:23:34 +0600

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,25 @@
+num=(2,34,67.89,67,1)
+x=34
+i=0
+while i<len(num):
+    if(num[i]==x):
+     print("I found index at",i)
+    i+=1
+
+for el in num:
+     
+    if(el ==x):
+        
+         print("Yes i found")
+        
+#print number 100 to 1
+for i in range(100,0,-1):
+     print(i)
+
+# Factorial of a number    
+sum=0
+a=5
+for i in range(1,a+1,+1):
+    sum=sum+i
+    print(i)
+print(sum)    
