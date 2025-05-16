2eb5c38c9781523b16cdedc462680911f5966b7f
Md Salauddin <145477267+456gdhj@users.noreply.github.com>
2025-05-16 22:43:16 +0600

    Update Read file.py

--- Read file.py
+++ Read file.py
@@ -1,9 +1,14 @@
-f = open("E:\Machine Learning\demo.txt","r")
-#data = f.read()
-#print(data)
+f = open("E:\Machine Learning\demo.txt", "r")
 
-#data=f.read(5)
-#print(data)
+data = f.read()    #Reads the entire file
+print(data)
 
-line1=f.readline()
+f.seek(0)          #Moves file pointer back to the beginning
+data = f.read(5)   #Reads the first 5 characters
+print(data)
+
+f.seek(0)           
+line1 = f.readline()  # reads the first line
 print(line1)
+
+f.close()
